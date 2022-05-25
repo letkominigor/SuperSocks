35628e9e3fe1531f083227c6a6706000d8541bac
Costa <kskrheh@gmail.com>
2022-05-25 20:23:11 +0300

    Pulled db, creating user in db, added bcrypt password

--- routes/views/login.router.js
+++ routes/views/login.router.js
@@ -2,3 +2,7 @@ const router = require('express').Router();
 const React = require('react');
 const ReactDOMServer = require('react-dom/server');
 const LoginPage = require('../../views/LoginPage');
+
+router.get('/', (req,res)=> {
+  const loginPage = React.createElement()
+})
--- routes/views/register.router.js
+++ routes/views/register.router.js
@@ -1,7 +1,12 @@
 const router = require('express').Router();
+const session = require('express-session');
 const React = require('react');
 const ReactDOMServer = require('react-dom/server');
+const bcrypt = require('bcrypt');
 const RegisterPage = require('../../views/RegisterPage');
+
+const { User } = require('../../Database/models');
+const { SALT_ROUNDS } = require('../../config/config');
 
 router.get('/', (req, res) => {
   const registerForm = React.createElement(RegisterPage);
@@ -9,9 +14,11 @@ router.get('/', (req, res) => {
   res.end(html);
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // register user in db
-  // const user = User.create({ username: req.body.username, password: req.body.password });
+  const cryptedPassword = await bcrypt.hash(req.body.password, SALT_ROUNDS);
+  const user = await User.create({ login: req.body.username, password: cryptedPassword });
+  req.session.user = user;
   res.redirect('/');
 });
 
