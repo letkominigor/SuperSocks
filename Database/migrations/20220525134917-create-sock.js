823c81129ae4d9ff7c5ae569a70614f3a1aa0baa
Сергей Цой <sergejcoj@MacBook-Pro-Sergej-2.local>
2022-05-25 20:09:34 +0300

    database comlited

---
+++ Database/migrations/20220525134917-create-sock.js
@@ -0,0 +1,43 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Socks', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      color: {
+        allowNull: false,
+        type: Sequelize.TEXT,
+      },
+      picture: {
+        allowNull: false,
+        type: Sequelize.TEXT,
+      },
+      pattern: {
+        allowNull: false,
+        type: Sequelize.TEXT,
+      },
+      user_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id',
+        },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface) {
+    await queryInterface.dropTable('Socks');
+  },
+};
