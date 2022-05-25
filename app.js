98c29e9f82ab002ae4a0361c9c886d022c9b9168
Costa <kskrheh@gmail.com>
2022-05-25 16:43:46 +0300

    Added react react-dom @babel/register

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.end('Privet ХУЙ');
+  res.end('Privet huj');
 });
 
 app.listen(PORT, () => {
---
+++ views/Layout.jsx
@@ -0,0 +1,15 @@
+const React = require('react');
+
+module.exports = function Layout() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Super Socks</title>
+      </head>
+      <body />
+    </html>
+  );
+};
