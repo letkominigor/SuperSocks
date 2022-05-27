db75b6284edbc31980f9b0b0388d8b09e9a838e4
Costa <kskrheh@gmail.com>
2022-05-27 16:31:19 +0300

    added margins on navbar

--- views/Navbar.jsx
+++ views/Navbar.jsx
@@ -8,20 +8,20 @@ module.exports = function Navbar({ user }) {
           ? (
             <nav>
               <ul style={{ display: 'flex', flexDirection: 'row', listStyleType: 'none' }}>
-                <li><a href="/">Logo</a></li>
-                <li><a href="/logout" className="js-logout">Logout</a></li>
-                <li><a href="/creator" className="js-creator">Generate a sock</a></li>
-                <li><a href="/socks-list" className="js-socks-list">Your socks</a></li>
+                <li style={{ marginRight: 20 }}><a href="/">Logo</a></li>
+                <li style={{ marginRight: 20 }}><a href="/logout" className="js-logout">Logout</a></li>
+                <li style={{ marginRight: 20 }}><a href="/creator" className="js-creator">Generate a sock</a></li>
+                <li style={{ marginRight: 20 }}><a href="/socks-list" className="js-socks-list">Your socks</a></li>
               </ul>
             </nav>
           )
           : (
             <nav>
               <ul style={{ display: 'flex', flexDirection: 'row', listStyleType: 'none' }}>
-                <li><a href="/">Logo</a></li>
-                <li><a href="/register" className="js-register-link">Register</a></li>
-                <li><a href="/login" className="js-login-link">Login</a></li>
-                <li><a href="/creator" className="js-creator">Generate a sock</a></li>
+                <li style={{ marginRight: 20 }}><a href="/">Logo</a></li>
+                <li style={{ marginRight: 20 }}><a href="/register" className="js-register-link">Register</a></li>
+                <li style={{ marginRight: 20 }}><a href="/login" className="js-login-link">Login</a></li>
+                <li style={{ marginRight: 20 }}><a href="/creator" className="js-creator">Generate a sock</a></li>
               </ul>
             </nav>
           )
